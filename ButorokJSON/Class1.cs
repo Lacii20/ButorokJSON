6b5580062b5ff58d1eb3a937483e68df70044c2c
raczl <raczl@kkszki.hu>
2026-02-23 07:53:42 +0100

    osztalyok

---
+++ ButorokJSON/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButorokJSON
+{
+    internal class Meretek
+    {
+        public int szelesseg {  get; set; }
+
+        public int magassag { get; set; }
+
+        public int melyseg { get; set; }
+    }
+}
---
+++ ButorokJSON/Class2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButorokJSON
+{
+    public class Targyak
+    {
+        public int azonosito {  get; set; }
+
+        public int nev { get; set; }
+
+        public int anyag { get; set; }
+
+        public int ar { get; set; }
+
+        public bool keszleten { get; set; }
+    }
+}
